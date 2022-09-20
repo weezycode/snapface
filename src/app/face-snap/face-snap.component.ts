ed1cf0b68e9de5f694ef3267a70dffa6d7d686ac
dicko <dickoafv@gmail.com>
2022-09-20 15:56:49 +0200

    start Angular

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
